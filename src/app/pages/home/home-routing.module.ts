49df35634bf7ecb46e04bb8f51f8f15567ddd555
Thanh Le <thanhle981998@gmail.com>
2020-05-29 01:59:50 -0700

    Updated UI designs

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -39,11 +39,11 @@ const routes: Routes = [
         [
           {
             path: '',
-            loadChildren: () => import('../menu/menu.module').then(m => m.MenuPageModule)
+            loadChildren: () => import('../menu-tab/menu/menu.module').then(m => m.MenuPageModule)
           },
           {
             path: 'manage-account',
-            loadChildren: () => import('../manage-account/manage-account.module').then( m => m.ManageAccountPageModule)
+            loadChildren: () => import('../menu-tab/manage-account/manage-account.module').then( m => m.ManageAccountPageModule)
           },
           {
             path: 'manage-tips',
@@ -51,11 +51,11 @@ const routes: Routes = [
             [
               {
                 path: '',
-                loadChildren: () => import('../manage-tips/manage-tips.module').then( m => m.ManageTipsPageModule)
+                loadChildren: () => import('../menu-tab/manage-tips/manage-tips.module').then( m => m.ManageTipsPageModule)
               },
               {
                 path: 'edit-tip',
-                loadChildren: () => import('../edit-tip/edit-tip.module').then( m => m.EditTipPageModule)
+                loadChildren: () => import('../menu-tab/edit-tip/edit-tip.module').then( m => m.EditTipPageModule)
               },
             ]
             
