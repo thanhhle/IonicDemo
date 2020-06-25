1d5cf0e8e8f1bf08e274b1cfd24a1968e315b4db
Thanh Le <thanhle981998@gmail.com>
2020-06-25 06:32:05 -0700

    Added Shopping Cart function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home/product-categories/accessories',
+    redirectTo: 'home/product-categories',
     pathMatch: 'full'
   },
   {
