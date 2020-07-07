5a39396f5dc74577211d2209b3fcb645ea3915d2
Thanh Le <thanhle981998@gmail.com>
2020-07-07 03:06:50 -0700

    Added Check Out UI design

--- src/app/pages/me-tab/check-out/check-out.page.ts
+++ src/app/pages/me-tab/check-out/check-out.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SimpleChanges, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { CartDataService } from '../../../services/cart-data.service'
